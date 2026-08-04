7ef106086eed4cdfdf09836b49bff4e3914916d9
KotsuCoder <whoami@kotsu.red>
2026-08-04 15:09:42 -0700

    Basic website layout put together.

---
+++ app.js
@@ -0,0 +1,7 @@
+document.getElementById('submit').addEventListener('click', function() {
+    document.getElementById('startform').style.display = 'none';
+});
+
+document.getElementById('config').addEventListener('click', function() {
+    document.getElementById('startform').style.display = 'grid';
+})
