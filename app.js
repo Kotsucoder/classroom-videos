e361bbcf2c46e9e758f564aadbf2ccd00c97ffa2
KotsuCoder <whoami@kotsu.red>
2026-08-04 18:37:43 -0700

    Added form to website.

--- app.js
+++ app.js
@@ -4,4 +4,16 @@ document.getElementById('submit').addEventListener('click', function() {
 
 document.getElementById('config').addEventListener('click', function() {
     document.getElementById('startform').style.display = 'grid';
-})
+});
+
+document.getElementById('birthdayYes').addEventListener('change', function() {
+    if (document.getElementById('birthdayYes').checked) {
+        document.getElementById('birthdayStudentField').classList.remove('hidden');
+    }
+});
+
+document.getElementById('birthdayNo').addEventListener('change', function() {
+    if (document.getElementById('birthdayNo').checked) {
+        document.getElementById('birthdayStudentField').classList.add('hidden');
+    }
+});
